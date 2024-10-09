d2c595c0f4f4b1305b39a8221f68f89de6f58496
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-09 14:00:37 +0200

    AddAccommodationModel

---
+++ HolidayResort.Domain/Entities/Accommodation.cs
@@ -0,0 +1,22 @@
+﻿namespace HolidayResort.Domain.Entities;
+
+public class Accommodation
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public double Price { get; set; }
+
+    public int SquareMeter { get; set;}
+
+    public int Capacity { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public DateTime CreatedDate { get; set;}
+
+    public DateTime UpdatedDate { get; set; }
+}
