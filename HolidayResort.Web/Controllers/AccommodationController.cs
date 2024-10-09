34bb42026d3bfb3d238d7344b009d48593e07257
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-09 15:34:52 +0200

    Create Accommodation Controller and Retrieve All Accommodation Data

---
+++ HolidayResort.Web/Controllers/AccommodationController.cs
@@ -0,0 +1,20 @@
+﻿using HolidayResort.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolidayResort.Web.Controllers;
+
+public class AccommodationController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AccommodationController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var accommodations = _context.Accommodations.ToList();
+        return View();
+    }
+}
