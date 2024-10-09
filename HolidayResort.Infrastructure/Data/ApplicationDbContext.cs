34585784ec49fd8afa8bacdb14347308dc3eb974
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-09 15:07:57 +0200

    CreateAccommodationTableInSql

--- HolidayResort.Infrastructure/Data/ApplicationDbContext.cs
+++ HolidayResort.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HolidayResort.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace HolidayResort.Infrastructure.Data;
 
@@ -7,4 +8,6 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Accommodation> Accommodations { get; set; }
 }
