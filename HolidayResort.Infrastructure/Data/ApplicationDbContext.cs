1d634d893f8753e017056cdf5f4a33d6d68e328f
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-09 14:15:47 +0200

    CreateDbContext

---
+++ HolidayResort.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HolidayResort.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
