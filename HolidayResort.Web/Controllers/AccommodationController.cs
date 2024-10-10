b45dce26ac69b6769e737026123d5872d71bb004
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-10 09:52:52 +0200

    ServerSideValidation

--- HolidayResort.Web/Controllers/AccommodationController.cs
+++ HolidayResort.Web/Controllers/AccommodationController.cs
@@ -27,8 +27,12 @@ public class AccommodationController : Controller
     [HttpPost]
     public IActionResult Create(Accommodation obj)
     {
-        _context.Accommodations.Add(obj);
-        _context.SaveChanges();
-        return RedirectToAction("Index");
+        if (ModelState.IsValid)
+        {
+            _context.Accommodations.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
     }
 }
