025124fddea966ef48c79d67b455c6374a3b9343
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-10 09:37:36 +0200

    DisplayDataAnnotation

--- HolidayResort.Domain/Entities/Accommodation.cs
+++ HolidayResort.Domain/Entities/Accommodation.cs
@@ -1,19 +1,21 @@
-﻿namespace HolidayResort.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HolidayResort.Domain.Entities;
 
 public class Accommodation
 {
     public int Id { get; set; }
-
+    [Display(Name="Naziv")]
     public required string Name { get; set; }
-
+    [Display(Name = "Opis")]
     public string? Description { get; set; }
-
+    [Display(Name = "Cijena po noćenju")]
     public double Price { get; set; }
-
+    [Display(Name = "Kvadratura")]
     public int SquareMeter { get; set;}
-
+    [Display(Name = "Kapacitet")]
     public int Capacity { get; set; }
-
+    [Display(Name = "Slika")]
     public string? ImageUrl { get; set; }
 
     public DateTime CreatedDate { get; set;}
