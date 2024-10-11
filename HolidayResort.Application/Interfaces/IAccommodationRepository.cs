69c0e51e477c9d259cd1e9dbed165ec581214dcf
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-11 11:59:47 +0200

    AccommodationInterface

---
+++ HolidayResort.Application/Interfaces/IAccommodationRepository.cs
@@ -0,0 +1,19 @@
+﻿using HolidayResort.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace HolidayResort.Application.Interfaces;
+
+public interface IAccommodationRepository
+{
+    IEnumerable<Accommodation> GetAll(Expression<Func<Accommodation,bool>>? filter = null, string? includeProperties = null);
+
+    IEnumerable<Accommodation> Get(Expression<Func<Accommodation, bool>>? filter, string? includeProperties = null);
+
+    void Create(Accommodation entity);
+
+    void Update(Accommodation entity);
+
+    void Delete(Accommodation entity);
+
+    void Save(Accommodation entity);
+}
