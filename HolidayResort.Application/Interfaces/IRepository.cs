20090017116516641c20500eb9096ac76449c0a0
hvalecdav <hvalecdav@HVALEC-DAV>
2024-10-11 12:50:33 +0200

    GenericRepository

---
+++ HolidayResort.Application/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using HolidayResort.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace HolidayResort.Application.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll(Expression<Func<Accommodation, bool>>? filter = null, string? includeProperties = null);
+
+    T Get(Expression<Func<Accommodation, bool>>? filter, string? includeProperties = null);
+
+    void Add(T entity);
+
+    void Remove(T entity);
+}
---
+++ HolidayResort.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,28 @@
+﻿using HolidayResort.Application.Interfaces;
+using HolidayResort.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace HolidayResort.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    public void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Get(Expression<Func<Accommodation, bool>>? filter, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll(Expression<Func<Accommodation, bool>>? filter = null, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
